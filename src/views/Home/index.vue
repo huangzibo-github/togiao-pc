3e879b43b766576142d17de8931c69825c04621d
xiaohuang <965650203@qq.com>
2020-04-27 13:55:46 +0800

    黑马头条PC-主页模块-页面布局-左侧导航菜单

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -2,7 +2,9 @@
     <div class="home">
         <el-container>
             <!-- 左右布局 -->
-            <el-aside class="aside">左侧</el-aside>
+            <el-aside style="background-color:#353b4e;height:100vh;width:230px">
+                <layout-aside></layout-aside>
+            </el-aside>
             <el-container>
                 <!-- 上下布局 -->
                 <el-header class="header">头部</el-header>
@@ -13,18 +15,16 @@
 </template>
 
 <script>
+import layoutAside from '../../components/home/layout-aside.vue'
 export default {
-
+  components: {
+    'layout-aside': layoutAside
+  }
 }
 </script>
 
 <style lang="less" scoped>
     .home {
-        .aside {
-            background-color: pink;
-            height: 100vh;
-        }
-
         .header {
             background-color: yellow;
         }
