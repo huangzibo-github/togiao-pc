943d9221b163c1ce73e904250b39a7aac8c333f7
xiaohuang <965650203@qq.com>
2020-04-27 12:36:59 +0800

    黑马头条PC-主页模块-页面布局

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,7 +1,15 @@
 <template>
-  <div class="home">
-      首页
-  </div>
+    <div class="home">
+        <el-container>
+            <!-- 左右布局 -->
+            <el-aside class="aside">左侧</el-aside>
+            <el-container>
+                <!-- 上下布局 -->
+                <el-header class="header">头部</el-header>
+                <el-main class="main">主要区域</el-main>
+            </el-container>
+        </el-container>
+    </div>
 </template>
 
 <script>
@@ -10,6 +18,19 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+    .home {
+        .aside {
+            background-color: pink;
+            height: 100vh;
+        }
 
+        .header {
+            background-color: yellow;
+        }
+
+        .main {
+            background-color: green;
+        }
+    }
 </style>
