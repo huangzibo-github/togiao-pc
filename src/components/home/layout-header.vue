513e708bebe911f2c341f7d190ef22a25df5f3f6
xiaohuang <965650203@qq.com>
2020-04-27 16:27:50 +0800

    黑马头条-主页模块-头部退出系统

--- src/components/home/layout-header.vue
+++ src/components/home/layout-header.vue
@@ -9,19 +9,19 @@
       <el-col class="right" :span='4'>
           <el-row type='flex' justify='end' align='middle'>
             <!-- 使用三元表达式，如果没有头像则使用默认头像 -->
-            <img :src="!userInfo.photo ? userInfo.photo : defaultImg " alt="">
+            <img :src="userInfo.photo ? userInfo.photo : defaultImg " alt="">
             <!-- 下拉菜单 -->
             <!-- 点击激活，设置trigger="click"，默认为hover激活 -->
-            <el-dropdown trigger="click">
+            <el-dropdown @command="handleCommand" trigger="click">
                 <span>
                     <!-- 更改昵称 -->
                     {{userInfo.name}}
                     <i class="el-icon-caret-bottom"></i>
                 </span>
                 <el-dropdown-menu slot="dropdown">
-                    <el-dropdown-item>个人信息</el-dropdown-item>
-                    <el-dropdown-item>git地址</el-dropdown-item>
-                    <el-dropdown-item>退出</el-dropdown-item>
+                    <el-dropdown-item command="info">个人信息</el-dropdown-item>
+                    <el-dropdown-item command="git">git地址</el-dropdown-item>
+                    <el-dropdown-item command="exit">退出</el-dropdown-item>
                 </el-dropdown-menu>
             </el-dropdown>
           </el-row>
@@ -50,6 +50,16 @@ export default {
       console.log(result.data)
       this.userInfo = result.data.data // 获取用户个人信息
     })
+  },
+  methods: {
+    handleCommand (command) {
+      if (command === 'exit') {
+        window.localStorage.removeItem('user-token') // 删除令牌
+        this.$router.push('/login') // 跳转回登录页
+      } else if (command === 'git') {
+        window.location.href = 'https://github.com/shuiruohanyu/89heimatoutiao'
+      }
+    }
   }
 }
 </script>
