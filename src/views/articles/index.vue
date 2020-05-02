8bbf76d05cf128703313fe69dee4aa2663a17e34
xiaohuang <965650203@qq.com>
2020-05-02 13:46:41 +0800

    黑马头条PC-内容列表-页面主体结构

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -46,6 +46,31 @@
             </el-date-picker>
         </el-col>
       </el-row>
+<!-- 主体结构 -->
+    <el-row class="total">
+        <span>共找到xx条符合条件的数据</span>
+    </el-row>
+    <!-- 循环模板 -->
+    <el-row v-for="item in 10" :key='item' class="articleItem" type='flex' justify='space-between'>
+        <!-- 左侧 -->
+        <el-col :span='14'>
+            <el-row type='flex' span='10'>
+                <img src="../../assets/img/404.png" alt="">
+                <div class="info">
+                    <span>xxxxx</span>
+                    <el-tag class="tag">已发表</el-tag>
+                    <span class="date">2019-12-12 09:15:20</span>
+                </div>
+            </el-row>
+        </el-col>
+        <!-- 右侧 -->
+        <el-col class="right" :span='6'>
+            <el-row type='flex' justify='end'>
+                <span><i class="el-icon-edit"></i>修改</span>
+                <span><i class="el-icon-delete"></i>删除</span>
+            </el-row>
+        </el-col>
+    </el-row>
   </el-card>
 </template>
 
@@ -81,5 +106,42 @@ export default {
     .searchTool {
         height: 60px;
         padding-left: 50px;
+    }
+    .total {
+        margin: 50px 0;
+        height: 20px;
+        border-bottom: 1px dashed #ccc;
+    }
+    .articleItem {
+        margin: 20px 0;
+        padding: 10px 0;
+        border-bottom: 1px solid #f2f3f5;
+        img {
+            width: 180px;
+            height: 120px;
+            margin-right: 10px;
+            border-radius: 4px;
+        }
+        .info {
+            height: 100px;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            .tag {
+                max-width: 80px;
+                text-align: center;
+            }
+            .date {
+                color: #999;
+                font-size: 12px;
+            }
+        }
+        .right {
+            span {
+                margin-left: 8px;
+                font-style: 14px;
+                cursor: pointer;
+            }
+        }
     }
 </style>
