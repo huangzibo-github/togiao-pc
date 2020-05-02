e8b2cd88449763ccad6841e736f0cba37c9899f1
xiaohuang <965650203@qq.com>
2020-05-02 12:48:35 +0800

    黑马头条PC-内容列表-搜索工具栏内容显示

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -9,12 +9,13 @@
           </el-col>
           <!-- 单选框组 -->
           <el-col :span="18">
-            <el-radio-group>
-                <el-radio>全部</el-radio>
-                <el-radio>草稿</el-radio>
-                <el-radio>待审核</el-radio>
-                <el-radio>审核通过</el-radio>
-                <el-radio>审核失败</el-radio>
+            <el-radio-group  v-model="formData.status">
+                <!-- 全部这个label='5',在传参的时候会判断一下是不是5，如果是5,则返回null -->
+                <el-radio :label='5'>全部</el-radio>
+                <el-radio :label='0'>草稿</el-radio>
+                <el-radio :label='1'>待审核</el-radio>
+                <el-radio :label='2'>审核通过</el-radio>
+                <el-radio :label='3'>审核失败</el-radio>
             </el-radio-group>
           </el-col>
       </el-row>
@@ -24,7 +25,10 @@
           <span>频道列表</span>
         </el-col>
         <el-col :span="18">
-            <el-select></el-select>
+            <el-select v-model="formData.channel_id">
+                <!-- 循环生成多个 label指的是el-option的显示值  value指的是el-option的储存值 -->
+                <el-option v-for="item in channels" :key='item.id' :label='item.name' :value='item.id'></el-option>
+            </el-select>
         </el-col>
       </el-row>
 <!-- 时间选择 -->
@@ -34,8 +38,9 @@
         </el-col>
         <el-col :span="18">
             <el-date-picker
+                v-model="formData.dateRange"
                 type="daterange"
-                range-separator="至"
+                range-separator="-"
                 start-placeholder="开始日期"
                 end-placeholder="结束日期">
             </el-date-picker>
@@ -46,7 +51,29 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      formData: {
+        status: 5,
+        channel_id: null,
+        dateRange: []
+      },
+      channels: []
+    }
+  },
+  methods: {
+    //   获取频道
+    getChannels () {
+      this.$axios({
+        url: '/channels'
+      }).then(result => {
+        this.channels = result.data.channels
+      })
+    }
+  },
+  created () {
+    this.getChannels()
+  }
 }
 </script>
 
