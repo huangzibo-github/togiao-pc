eefd3b8c523a670e4facf38a3560a3158e68e1a7
xiaohuang <965650203@qq.com>
2020-05-05 14:37:48 +0800

    黑马头条PC-发表文章-注册富文本编辑器

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,16 @@
 import layoutAside from './home/layout-aside'
 import layoutHeader from './home/layout-header'
 import breadCrumb from './common/bread-crumb'
+import { quillEditor } from 'vue-quill-editor' // 引入quill组件
+import 'quill/dist/quill.core.css' // 引入富文本编辑器样式
+import 'quill/dist/quill.snow.css' // 引入富文本编辑器样式
+import 'quill/dist/quill.bubble.css' // 引入富文本编辑器样式
 // 所有自定义组件的插件
 export default {
   install (Vue) { // 调用该对象的install方法
     Vue.component('layout-aside', layoutAside)
     Vue.component('layout-header', layoutHeader)
     Vue.component('bread-crumb', breadCrumb) // 全局注册一个面包屑组件
+    Vue.component('quill-editor', quillEditor) // 全局注册富文本编辑器
   }
 }
--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -9,9 +9,9 @@
               <el-input v-model="formData.title" style="width:60%"></el-input>
           </el-form-item>
           <el-form-item prop="content" label="内容">
-              <el-input v-model="formData.content" type='textarea' :row='4'></el-input>
+              <quill-editor style="height:400px" v-model="formData.content" type='textarea' :row='4'></quill-editor>
           </el-form-item>
-          <el-form-item prop="type" label="封面">
+          <el-form-item prop="type" label="封面" style="margin-top:100px">
               <el-radio-group v-model="formData.cover.type">
                   <el-radio :label="1">单图</el-radio>
                   <el-radio :label="3">三图</el-radio>
@@ -102,6 +102,7 @@ export default {
           //     this.$router.push('/home/articles')
           //   })
           // }
+          // 代码优化！！---------------!!！
           this.$axios({
             method: articleId ? 'put' : 'post',
             url: articleId ? `/articles/${articleId}` : '/articles',
