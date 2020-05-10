e4b5d68faafc28f3fe50b4f29e37209a80ca5fed
xiaohuang <965650203@qq.com>
2020-05-10 15:04:04 +0800

    黑马头条PC-404页面应用

---
+++ src/views/404.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="not-found">
+      <img src="../assets/img/404.png" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+    .not-found {
+        height: 100vh;
+        width: 200vh;
+        img {
+            width: 100%;
+            height: 100%;
+        }
+    }
+</style>
